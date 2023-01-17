e72c96830e0c10cef2a9d617e9cea1e93bea3639
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-01-17 13:06:07 +0100

    some chancges to make node functioning

---
+++ public/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/html/prisustvo.html'));
+ });
+ 
