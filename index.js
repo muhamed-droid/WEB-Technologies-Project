747dff1ee7d7bf4fae6a32401d906ad50452f7b1
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-01-18 11:57:17 +0100

    uspješno pokrenute stranice preko node-a

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/html/prisustvo.html'));
+	res.sendFile(path.join(__dirname, 'public' , 'html' , 'predmet.html'));
  });
+
+
+
+
  
